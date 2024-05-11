ac159aad3df6bc5c1f4c395caf37189b94cf6173
maxpp2910 <107290604+maxpp2910@users.noreply.github.com>
2024-05-11 17:39:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ $(function(){
   //add an image bucle to show diferent images every 5 seconds
   var images = [
     'https://media.discordapp.net/attachments/1227836073778876450/1238979676039282698/melany.png?ex=6641413d&is=663fefbd&hm=636799571b90dd590efd381385d84a9cf43000fb97694cc85ee7bd6580efba06&=&format=webp&quality=lossless&width=597&height=597',
-    'https://media.discordapp.net/attachments/1227836073778876450/1238979676563705876/tony.png?ex=6641413e&is=663fefbe&hm=20f2dce09237b29c56aa22ce8b165b8bdabd7d5f2ad9c5df6fcd3e3e12048b11&=&format=webp&quality=lossless&width=597&height=597'
+    'https://media.discordapp.net/attachments/1227836073778876450/1238983669310427258/Tony.png?ex=664144f5&is=663ff375&hm=dbe8cb1bb973c3b32792dcf9bf342a7a6894e4d18762a10c03a88d1c5d4a9703&=&format=webp&quality=lossless&width=597&height=597'
 ];
 
 var counter = 0;
